32e01bfd4be13b834b899cb18ed414f14b93b133
StewStrong <stew.strong.420@gmail.com>
2023-10-19 07:44:03 -0600

    Only disable contraption collision if a shadow ship exists

--- common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinContraptionCollider.java
+++ common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinContraptionCollider.java
@@ -6,6 +6,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.create_interactive.mixinducks.AbstractContraptionEntityDuck;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
 
 @Mixin(ContraptionCollider.class)
 public class MixinContraptionCollider {
@@ -13,7 +16,12 @@ public class MixinContraptionCollider {
      * Disable contraption collision entirely! We will get it from VS2 instead!
      */
     @Inject(method = "collideEntities", at = @At("HEAD"), cancellable = true, remap = false)
-    private static void preCollideEntities(AbstractContraptionEntity contraptionEntity, CallbackInfo ci) {
+    private static void preCollideEntities(final AbstractContraptionEntity contraptionEntity, final CallbackInfo ci) {
+        // Only disable collision if the ship isn't here yet
+        final Long shipId = ((AbstractContraptionEntityDuck) contraptionEntity).getShadowShipId();
+        if (shipId == null) return;
+        final Ship ship = VSGameUtilsKt.getShipObjectWorld(contraptionEntity.level).getAllShips().getById(shipId);
+        if (ship == null) return;
         ci.cancel();
     }
 }
