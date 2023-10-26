c92090197f5ffc10313bc277b2511a1344faafa8
StewStrong <stewstronger@gmail.com>
2023-10-26 05:54:30 -0600

    Moved MixinTrain logic to MixinTrainLogic

--- common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinTrain.java
+++ common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinTrain.java
@@ -1,113 +1,23 @@
 package org.valkyrienskies.create_interactive.mixin;
 
-import com.simibubi.create.content.trains.entity.Carriage;
-import com.simibubi.create.content.trains.entity.CarriageContraptionEntity;
 import com.simibubi.create.content.trains.entity.Train;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.valkyrienskies.create_interactive.mixin_logic.MixinTrainLogic;
 import org.valkyrienskies.create_interactive.mixinducks.TrainDuck;
-
-import java.util.List;
 
 @Mixin(Train.class)
 public abstract class MixinTrain implements TrainDuck {
-    @Shadow
-    public List<Carriage> carriages;
-    @Shadow
-    public List<Integer> carriageSpacing;
-
-    @Shadow
-    public abstract boolean disassemble(Direction assemblyDirection, BlockPos pos);
-
     // Allow trains to disassemble when they have no blocks
     @Inject(method = "canDisassemble", at = @At("HEAD"), cancellable = true, remap = false)
     private void preCanDisassemble(final CallbackInfoReturnable<Boolean> cir) {
-        for (final Carriage carriage : carriages) {
-            final CarriageContraptionEntity entity = carriage.anyAvailableEntity();
-            if (entity == null) {
-                return;
-            }
-            if (!entity.getContraption().getBlocks().isEmpty()) {
-                return;
-            }
-        }
-        cir.setReturnValue(true);
+        MixinTrainLogic.INSTANCE.preCanDisassemble(Train.class.cast(this), cir);
     }
 
     @Override
     public void ci$splitOrDisassemble() {
-        for (int i = 0; i < carriages.size(); i++) {
-            final Carriage carriage = carriages.get(i);
-            final CarriageContraptionEntity entity = carriage.anyAvailableEntity();
-            if (entity == null) {
-                return;
-            }
-            if (entity.getContraption().getBlocks().isEmpty()) {
-                if (carriages.size() == 1) {
-                    final BlockPos pos = new BlockPos(entity.position());
-                    disassemble(Direction.NORTH, pos);
-                } else if (i == 0) {
-                    // Remove first car
-                    final Carriage firstCar = carriages.remove(0);
-                    carriageSpacing.remove(0);
-                    firstCar.setTrain(null);
-                    entity.kill();
-                } else if (i == carriages.size() - 1) {
-                    // Remove last car
-                    final Carriage lastCar = carriages.remove(carriages.size() - 1);
-                    carriageSpacing.remove(carriages.size() - 1);
-                    lastCar.setTrain(null);
-                    entity.kill();
-                } else {
-                    /* Disabled because this is broken
-                    // Split train
-
-                    final Train thisAs = Train.class.cast(this);
-
-                    final List<Carriage> newCarriages = new ArrayList<>();
-                    final List<Integer> newSpacing = new ArrayList<>();
-
-                    for (int j = i + 1; j < carriages.size(); j++) {
-                        newCarriages.add(carriages.get(j));
-                    }
-
-                    if (carriages.size() > i + 1) {
-                        carriages.subList(i + 1, carriages.size()).clear();
-                    }
-
-                    for (int j = i + 1; j < carriageSpacing.size(); j++) {
-                        newSpacing.add(carriageSpacing.get(j));
-                    }
-                    if (carriageSpacing.size() > i + 1) {
-                        carriageSpacing.subList(i + 1, carriageSpacing.size()).clear();
-                    }
-
-                    carriageSpacing.remove(i);
-                    carriageSpacing.remove(i - 1);
-
-                    final Carriage deadCar = carriages.remove(i);
-
-                    deadCar.setTrain(null);
-                    final Level level = entity.level;
-                    entity.kill();
-
-                    final Train newTrain = new Train(UUID.randomUUID(), thisAs.owner, thisAs.graph, newCarriages, newSpacing, thisAs.doubleEnded);
-                    newTrain.collectInitiallyOccupiedSignalBlocks();
-                    Create.RAILWAYS.addTrain(newTrain);
-                    AllPackets.getChannel().sendToClientsInServer(new TrainPacket(newTrain, true), level.getServer());
-
-                    // Update this train too
-                    AllPackets.getChannel().sendToClientsInServer(new TrainPacket(thisAs, true), level.getServer());
-                    return;
-
-                     */
-                }
-            }
-        }
+        MixinTrainLogic.INSTANCE.splitOrDisassemble(Train.class.cast(this));
     }
 }
---
+++ common/src/main/kotlin/org/valkyrienskies/create_interactive/mixin_logic/MixinTrainLogic.kt
@@ -0,0 +1,89 @@
+package org.valkyrienskies.create_interactive.mixin_logic
+
+import com.simibubi.create.content.trains.entity.Carriage
+import com.simibubi.create.content.trains.entity.Train
+import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
+
+internal object MixinTrainLogic {
+    fun preCanDisassemble(train: Train, cir: CallbackInfoReturnable<Boolean?>) {
+        for (carriage in train.carriages) {
+            val entity = carriage.anyAvailableEntity() ?: return
+            if (entity.contraption.blocks.isNotEmpty()) {
+                return
+            }
+        }
+        cir.setReturnValue(true)
+    }
+
+    fun splitOrDisassemble(train: Train) {
+        val carriages = train.carriages
+        val carriageSpacing = train.carriageSpacing
+        for (i in carriages.indices) {
+            val carriage: Carriage = carriages.get(i)
+            val entity = carriage.anyAvailableEntity() ?: return
+            if (entity.contraption.blocks.isEmpty()) {
+                if (carriages.size == 1) {
+                    val pos = BlockPos(entity.position())
+                    train.disassemble(Direction.NORTH, pos)
+                } else if (i == 0) {
+                    // Remove first car
+                    val firstCar: Carriage = carriages.removeAt(0)
+                    carriageSpacing.removeAt(0)
+                    firstCar.setTrain(null)
+                    entity.kill()
+                } else if (i == carriages.size - 1) {
+                    // Remove last car
+                    val lastCar: Carriage = carriages.removeAt(carriages.size - 1)
+                    carriageSpacing.removeAt(carriageSpacing.size - 1)
+                    lastCar.setTrain(null)
+                    entity.kill()
+                } else {
+                    /* Disabled because this is broken
+                    // Split train
+
+                    final Train thisAs = Train.class.cast(this);
+
+                    final List<Carriage> newCarriages = new ArrayList<>();
+                    final List<Integer> newSpacing = new ArrayList<>();
+
+                    for (int j = i + 1; j < carriages.size(); j++) {
+                        newCarriages.add(carriages.get(j));
+                    }
+
+                    if (carriages.size() > i + 1) {
+                        carriages.subList(i + 1, carriages.size()).clear();
+                    }
+
+                    for (int j = i + 1; j < carriageSpacing.size(); j++) {
+                        newSpacing.add(carriageSpacing.get(j));
+                    }
+                    if (carriageSpacing.size() > i + 1) {
+                        carriageSpacing.subList(i + 1, carriageSpacing.size()).clear();
+                    }
+
+                    carriageSpacing.remove(i);
+                    carriageSpacing.remove(i - 1);
+
+                    final Carriage deadCar = carriages.remove(i);
+
+                    deadCar.setTrain(null);
+                    final Level level = entity.level;
+                    entity.kill();
+
+                    final Train newTrain = new Train(UUID.randomUUID(), thisAs.owner, thisAs.graph, newCarriages, newSpacing, thisAs.doubleEnded);
+                    newTrain.collectInitiallyOccupiedSignalBlocks();
+                    Create.RAILWAYS.addTrain(newTrain);
+                    AllPackets.getChannel().sendToClientsInServer(new TrainPacket(newTrain, true), level.getServer());
+
+                    // Update this train too
+                    AllPackets.getChannel().sendToClientsInServer(new TrainPacket(thisAs, true), level.getServer());
+                    return;
+
+                     */
+                }
+            }
+        }
+    }
+}
