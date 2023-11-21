10cc1c289520e0ae202c0ab650c2ecf6300b3ea8
StewStrong <stewstronger@gmail.com>
2023-11-21 00:07:38 -0700

    Moved logic to MixinMinecraftLogic

--- common/src/main/java/org/valkyrienskies/create_interactive/mixin/client/MixinMinecraft.java
+++ common/src/main/java/org/valkyrienskies/create_interactive/mixin/client/MixinMinecraft.java
@@ -2,25 +2,14 @@ package org.valkyrienskies.create_interactive.mixin.client;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.simibubi.create.AllItems;
-import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
-import com.simibubi.create.content.contraptions.ContraptionHandler;
-import com.simibubi.create.content.contraptions.ContraptionHandlerClient;
-import com.simibubi.create.content.trains.entity.CarriageContraptionEntity;
-import com.simibubi.create.content.trains.entity.TrainRelocator;
-import com.simibubi.create.foundation.utility.Couple;
-import com.simibubi.create.foundation.utility.VecHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -29,8 +18,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.valkyrienskies.create_interactive.mixin_logic.client.MixinMinecraftLogic;
 import org.valkyrienskies.mod.common.IShipObjectWorldClientProvider;
 
-import java.lang.ref.WeakReference;
-
 // Set priority to 1001 to run these after VS2 mixins
 @Mixin(value = Minecraft.class, priority = 1001)
 public abstract class MixinMinecraft implements IShipObjectWorldClientProvider {
@@ -38,6 +25,7 @@ public abstract class MixinMinecraft implements IShipObjectWorldClientProvider {
     private boolean pause;
     @Shadow
     public ClientLevel level;
+
     @Shadow
     public abstract ClientPacketListener getConnection();
 
@@ -46,48 +34,29 @@ public abstract class MixinMinecraft implements IShipObjectWorldClientProvider {
         at = @At("RETURN")
     )
     public void postTick(final CallbackInfo ci) {
-        MixinMinecraftLogic.INSTANCE.postTick$create_interactive(pause, level, getConnection(), ci);
+        MixinMinecraftLogic.INSTANCE.postTick$create_interactive(pause, level, getConnection());
     }
 
     /**
      * Fix train wrenching by doing train wrenching after block interactions
      */
-    @WrapOperation(method = "startUseItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;useItemOn(Lnet/minecraft/client/player/LocalPlayer;Lnet/minecraft/client/multiplayer/ClientLevel;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/phys/BlockHitResult;)Lnet/minecraft/world/InteractionResult;"))
+    @WrapOperation(
+        method = "startUseItem",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;useItemOn(Lnet/minecraft/client/player/LocalPlayer;Lnet/minecraft/client/multiplayer/ClientLevel;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/phys/BlockHitResult;)Lnet/minecraft/world/InteractionResult;"
+        )
+    )
     private InteractionResult wrapUseItemOn(
         final MultiPlayerGameMode gameMode,
         final LocalPlayer player,
         final ClientLevel level,
         final InteractionHand interactionHand,
-        final BlockHitResult blockhitresult,
+        final BlockHitResult blockHitResult,
         final Operation<InteractionResult> operation
     ) {
-        final InteractionResult result = operation.call(gameMode, player, level, interactionHand, blockhitresult);
-        if (!result.consumesAction()) {
-            // Consider a train relocation
-            for (final WeakReference<AbstractContraptionEntity> contraptionEntityRef : ContraptionHandler.loadedContraptions.get(level).values()) {
-                final Couple<Vec3> rayInputs = ContraptionHandlerClient.getRayInputs(player);
-                final Vec3 origin = rayInputs.getFirst();
-                final Vec3 target = rayInputs.getSecond();
-                final AABB aabb = new AABB(origin, target).inflate(16);
-                final AbstractContraptionEntity contraptionEntity = contraptionEntityRef.get();
-                if (contraptionEntity == null)
-                    continue;
-                if (!contraptionEntity.getBoundingBox()
-                    .intersects(aabb))
-                    continue;
-
-                final BlockHitResult rayTraceResult = ContraptionHandlerClient.rayTraceContraption(origin, target, contraptionEntity);
-                if (rayTraceResult == null)
-                    continue;
-
-                final BlockPos pos = rayTraceResult.getBlockPos();
-
-                // Effectively do ContraptionHandlerClient.handleSpecialInteractions()
-                if (AllItems.WRENCH.isIn(player.getItemInHand(interactionHand)) && contraptionEntity instanceof CarriageContraptionEntity car) {
-                    TrainRelocator.carriageWrenched(car.toGlobalVector(VecHelper.getCenterOf(pos), 1), car);
-                }
-            }
-        }
-        return result;
+        return MixinMinecraftLogic.INSTANCE.wrapUseItemOn$create_interactive(
+            gameMode, player, level, interactionHand, blockHitResult, operation
+        );
     }
 }
--- common/src/main/kotlin/org/valkyrienskies/create_interactive/mixin_logic/client/MixinMinecraftLogic.kt
+++ common/src/main/kotlin/org/valkyrienskies/create_interactive/mixin_logic/client/MixinMinecraftLogic.kt
@@ -1,14 +1,66 @@
 package org.valkyrienskies.create_interactive.mixin_logic.client
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation
+import com.simibubi.create.AllItems
+import com.simibubi.create.content.contraptions.AbstractContraptionEntity
+import com.simibubi.create.content.contraptions.ContraptionHandler
+import com.simibubi.create.content.contraptions.ContraptionHandlerClient
+import com.simibubi.create.content.trains.entity.CarriageContraptionEntity
+import com.simibubi.create.content.trains.entity.TrainRelocator
+import com.simibubi.create.foundation.utility.VecHelper
 import net.minecraft.client.multiplayer.ClientLevel
 import net.minecraft.client.multiplayer.ClientPacketListener
+import net.minecraft.client.multiplayer.MultiPlayerGameMode
+import net.minecraft.client.player.LocalPlayer
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.phys.AABB
+import net.minecraft.world.phys.BlockHitResult
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 import org.valkyrienskies.create_interactive.CreateInteractiveEventsClient.postTickClient
+import java.lang.ref.WeakReference
 
 internal object MixinMinecraftLogic {
-    internal fun postTick(pause: Boolean, level: ClientLevel?, connection: ClientPacketListener?, ci: CallbackInfo?) {
+    internal fun postTick(pause: Boolean, level: ClientLevel?, connection: ClientPacketListener?) {
         if (!pause && level != null && connection != null) {
             postTickClient()
         }
+    }
+
+    internal fun wrapUseItemOn(
+        gameMode: MultiPlayerGameMode,
+        player: LocalPlayer,
+        level: ClientLevel,
+        interactionHand: InteractionHand,
+        blockHitResult: BlockHitResult,
+        operation: Operation<InteractionResult>,
+    ): InteractionResult {
+        val result = operation.call(gameMode, player, level, interactionHand, blockHitResult)
+        if (!result.consumesAction()) {
+            // Consider a train relocation
+            val contraptionsInLevel = ContraptionHandler.loadedContraptions.get(level).values
+            for (contraptionEntityRef: WeakReference<AbstractContraptionEntity> in contraptionsInLevel) {
+                val rayInputs = ContraptionHandlerClient.getRayInputs(player)
+                val origin = rayInputs.first
+                val target = rayInputs.second
+                val aabb = AABB(origin, target).inflate(16.0)
+                val contraptionEntity = contraptionEntityRef.get() ?: continue
+                if (!contraptionEntity.boundingBox.intersects(aabb))
+                    continue
+
+                val rayTraceResult =
+                    ContraptionHandlerClient.rayTraceContraption(origin, target, contraptionEntity) ?: continue
+                val pos = rayTraceResult.blockPos
+
+                // Effectively do ContraptionHandlerClient.handleSpecialInteractions()
+                if (AllItems.WRENCH.isIn(player.getItemInHand(interactionHand)) && contraptionEntity is CarriageContraptionEntity) {
+                    TrainRelocator.carriageWrenched(
+                        contraptionEntity.toGlobalVector(VecHelper.getCenterOf(pos), 1.0f),
+                        contraptionEntity
+                    )
+                }
+            }
+        }
+        return result
     }
 }
