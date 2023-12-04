98cd6df618d61b1a898ed58d7caaa182adcb87f9
StewStrong <stewstronger@gmail.com>
2023-12-03 22:12:36 -0700

    Fixed train derailment being weird at angles (wrenching not working, bogeys in wrong place)

--- common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinOrientedContraptionEntity.java
+++ common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinOrientedContraptionEntity.java
@@ -3,6 +3,7 @@ package org.valkyrienskies.create_interactive.mixin;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
 import com.simibubi.create.content.contraptions.OrientedContraptionEntity;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -33,6 +34,34 @@ public class MixinOrientedContraptionEntity implements OrientedContraptionEntity
     private void preApplyLocalTransforms(PoseStack matrixStack, float partialTicks, CallbackInfo ci) {
         MixinOrientedContraptionEntityLogic.INSTANCE.preApplyLocalTransforms$create_interactive(
             OrientedContraptionEntity.class.cast(this), matrixStack, ci
+        );
+    }
+
+    @Inject(method = "getViewXRot", at = @At("HEAD"), cancellable = true)
+    private void preGetViewXRot(float partialTicks, CallbackInfoReturnable<Float> cir) {
+        MixinOrientedContraptionEntityLogic.INSTANCE.preGetViewXRot$create_interactive(
+            OrientedContraptionEntity.class.cast(this), cir
+        );
+    }
+
+    @Inject(method = "getViewYRot", at = @At("HEAD"), cancellable = true)
+    private void preGetViewYRot(float partialTicks, CallbackInfoReturnable<Float> cir) {
+        MixinOrientedContraptionEntityLogic.INSTANCE.preGetViewYRot$create_interactive(
+            OrientedContraptionEntity.class.cast(this), cir
+        );
+    }
+
+    @Inject(method = "applyRotation", at = @At("HEAD"), cancellable = true)
+    private void preApplyRotation(Vec3 localPos, float partialTicks, CallbackInfoReturnable<Vec3> cir) {
+        MixinOrientedContraptionEntityLogic.INSTANCE.preApplyRotation$create_interactive(
+            OrientedContraptionEntity.class.cast(this), localPos, partialTicks, cir
+        );
+    }
+
+    @Inject(method = "reverseRotation", at = @At("HEAD"), cancellable = true)
+    private void preReverseRotation(Vec3 localPos, float partialTicks, CallbackInfoReturnable<Vec3> cir) {
+        MixinOrientedContraptionEntityLogic.INSTANCE.preReverseRotation$create_interactive(
+            OrientedContraptionEntity.class.cast(this), localPos, partialTicks, cir
         );
     }
 }
