41c995cc9a3aa85ceed338693a2efda9667c95b5
StewStrong <stewstronger@gmail.com>
2023-12-03 20:58:00 -0700

    Fixed bogeys not rendering correctly on derailed trains

---
+++ common/src/main/java/org/valkyrienskies/create_interactive/mixin/client/MixinCarriageContraptionEntityRenderer.java
@@ -0,0 +1,28 @@
+package org.valkyrienskies.create_interactive.mixin.client;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.simibubi.create.content.trains.entity.CarriageBogey;
+import com.simibubi.create.content.trains.entity.CarriageContraptionEntityRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.create_interactive.mixin_logic.client.MixinCarriageContraptionEntityRendererLogic;
+
+@Mixin(CarriageContraptionEntityRenderer.class)
+public class MixinCarriageContraptionEntityRenderer {
+    @Inject(method = "translateBogey", at = @At("HEAD"))
+    private static void preTranslateBogey(
+        final PoseStack ms,
+        final CarriageBogey bogey,
+        final int bogeySpacing,
+        final float viewYRot,
+        final float viewXRot,
+        final float partialTicks,
+        final CallbackInfo ci
+    ) {
+        MixinCarriageContraptionEntityRendererLogic.INSTANCE.preTranslateBogey$create_interactive(
+            ms, bogey, bogeySpacing, viewYRot, viewXRot, partialTicks, ci
+        );
+    }
+}
---
+++ common/src/main/kotlin/org/valkyrienskies/create_interactive/mixin_logic/client/MixinCarriageContraptionEntityRendererLogic.kt
@@ -0,0 +1,32 @@
+package org.valkyrienskies.create_interactive.mixin_logic.client
+
+import com.mojang.blaze3d.vertex.PoseStack
+import com.simibubi.create.content.trains.entity.CarriageBogey
+import net.minecraft.client.multiplayer.ClientLevel
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+import org.valkyrienskies.create_interactive.CreateInteractiveUtil
+import org.valkyrienskies.create_interactive.mixinducks.AbstractContraptionEntityDuck
+import org.valkyrienskies.mod.common.shipObjectWorld
+import org.valkyrienskies.mod.common.util.toMinecraft
+
+internal object MixinCarriageContraptionEntityRendererLogic {
+    internal fun preTranslateBogey(
+        ms: PoseStack,
+        bogey: CarriageBogey,
+        bogeySpacing: Int,
+        viewYRot: Float,
+        viewXRot: Float,
+        partialTicks: Float,
+        ci: CallbackInfo
+    ) {
+        val contraptionEntity = bogey.carriage.anyAvailableEntity()
+        val shipId = (contraptionEntity as AbstractContraptionEntityDuck).`ci$getShadowShipId`() ?: return
+        val clientShip = (contraptionEntity.level as ClientLevel).shipObjectWorld.allShips.getById(shipId) ?: return
+        if (CreateInteractiveUtil.isTrainDerailed(contraptionEntity)) {
+            val rotationTransform = clientShip.renderTransform.shipToWorldRotation
+            ms.translate(0.0, 0.5, 0.0)
+            ms.mulPose(rotationTransform.toMinecraft())
+            ms.translate(0.0, -0.5, 0.0)
+        }
+    }
+}
