dde2f933055b4ec2f9b14be8e9d2c3b39258b090
StewStrong <stewstronger@gmail.com>
2023-12-05 06:36:02 -0700

    An attempt at fixing trains being strange when nearby rails are broken

--- common/src/main/kotlin/org/valkyrienskies/create_interactive/mixin_logic/MixinContraptionRotationStateLogic.kt
+++ common/src/main/kotlin/org/valkyrienskies/create_interactive/mixin_logic/MixinContraptionRotationStateLogic.kt
@@ -6,10 +6,10 @@ import org.joml.Quaterniond
 internal object MixinContraptionRotationStateLogic {
     internal fun getRotationQuaternion(rotState: AbstractContraptionEntity.ContraptionRotationState, xRot: Float, yRot: Float, zRot: Float, dest: Quaterniond): Quaterniond {
         dest.set(0.0, 0.0, 0.0, 1.0)
-        val newRot = dest.rotateZYX(
-            Math.toRadians(zRot.toDouble()),
-            Math.toRadians(yRot.toDouble()),
+        val newRot = dest.rotateXYZ(
             Math.toRadians(xRot.toDouble()),
+            Math.toRadians(yRot.toDouble()),
+            Math.toRadians(zRot.toDouble()),
         )
         newRot.rotateLocalY(Math.toRadians(rotState.yawOffset.toDouble()))
         return newRot
