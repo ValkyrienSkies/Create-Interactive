249578286c91e58ee44da3ca54e5eff69317c7e4
StewStrong <stewstronger@gmail.com>
2023-12-21 07:20:30 -0700

    Make NoOptimize retention at source

--- common/src/main/kotlin/org/valkyrienskies/create_interactive/services/NoOptimize.kt
+++ common/src/main/kotlin/org/valkyrienskies/create_interactive/services/NoOptimize.kt
@@ -4,5 +4,5 @@ package org.valkyrienskies.create_interactive.services
  * Use this annotation to tell proguard pretty please do not delete my function!
  */
 @Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.RUNTIME)
+@Retention(AnnotationRetention.SOURCE)
 annotation class NoOptimize
