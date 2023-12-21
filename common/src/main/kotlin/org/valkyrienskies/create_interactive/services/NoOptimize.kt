0ce030bbd6fe2e0036f999762f5bf2025f128397
StewStrong <stewstronger@gmail.com>
2023-12-21 07:43:49 -0700

    Make NoOptimize retention at binary

--- common/src/main/kotlin/org/valkyrienskies/create_interactive/services/NoOptimize.kt
+++ common/src/main/kotlin/org/valkyrienskies/create_interactive/services/NoOptimize.kt
@@ -4,5 +4,5 @@ package org.valkyrienskies.create_interactive.services
  * Use this annotation to tell proguard pretty please do not delete my function!
  */
 @Target(AnnotationTarget.FUNCTION)
-@Retention(AnnotationRetention.SOURCE)
+@Retention(AnnotationRetention.BINARY)
 annotation class NoOptimize
