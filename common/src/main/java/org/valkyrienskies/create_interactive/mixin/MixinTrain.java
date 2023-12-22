12be4113e1831134efb0fd28e9f2ac1264d2c640
StewStrong <stewstronger@gmail.com>
2023-12-22 03:26:58 -0700

    Fixed forge not working

--- common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinTrain.java
+++ common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinTrain.java
@@ -1,12 +1,8 @@
 package org.valkyrienskies.create_interactive.mixin;
 
 import com.simibubi.create.content.trains.entity.Train;
-import com.simibubi.create.foundation.utility.Pair;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -27,26 +23,14 @@ public abstract class MixinTrain implements TrainDuck {
         MixinTrainLogic.INSTANCE.splitOrDisassemble$create_interactive(Train.class.cast(this));
     }
 
+    /**
+     * Derail trains when they hit the end of the track, unless there is a buffer stop to stop them.
+     */
     @Inject(
         method = "tick",
         at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/trains/entity/TrainStatus;endOfTrack()V")
     )
     private void tickOnEndOfTrack(final Level level, final CallbackInfo ci) {
         MixinTrainLogic.INSTANCE.tickOnEndOfTrack$create_interactive(Train.class.cast(this));
-    }
-
-    /**
-     * @author Triode
-     * @reason Don't collide trains with derailed trains
-     */
-    @Overwrite
-    public static Pair<Train, Vec3> findCollidingTrain(
-        Level level,
-        Vec3 start,
-        Vec3 end,
-        Train ignore,
-        ResourceKey<Level> dimension
-    ) {
-        return MixinTrainLogic.INSTANCE.findCollidingTrain$create_interactive(level, start, end, ignore, dimension);
     }
 }
