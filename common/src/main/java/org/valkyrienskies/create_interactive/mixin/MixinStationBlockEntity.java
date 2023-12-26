d6ea5a82118f0174a0dd7d866d268832e236b411
ewoudje <7384674+ewoudje@users.noreply.github.com>
2023-12-27 00:19:19 +0100

    Allow assembling without controls

---
+++ common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinStationBlockEntity.java
@@ -0,0 +1,20 @@
+package org.valkyrienskies.create_interactive.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.simibubi.create.content.trains.station.StationBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(StationBlockEntity.class)
+public class MixinStationBlockEntity {
+
+    // You aren't required to add controls to the trains since you can just add them afterwards
+    @WrapOperation(
+        method = "assemble",
+        at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/trains/entity/CarriageContraption;hasForwardControls()Z")
+    )
+    private boolean skipControls(boolean original) {
+        return true;
+    }
+}
