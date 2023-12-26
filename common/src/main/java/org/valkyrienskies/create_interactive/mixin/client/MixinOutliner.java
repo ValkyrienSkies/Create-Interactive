1d55ea49d83e454c938cae7bf1299fb657b58fc1
StewStrong <stewstronger@gmail.com>
2023-12-26 13:15:24 -0700

    Moved MixinOutliner to client

--- common/src/main/java/org/valkyrienskies/create_interactive/mixin/client/MixinOutliner.java
+++ common/src/main/java/org/valkyrienskies/create_interactive/mixin/client/MixinOutliner.java
@@ -1,4 +1,4 @@
-package org.valkyrienskies.create_interactive.mixin;
+package org.valkyrienskies.create_interactive.mixin.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.foundation.outliner.Outliner;
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.create_interactive.mixin_logic.client.MixinOutlinerLogic;
 
 import java.util.Map;
 
@@ -27,8 +28,6 @@ public class MixinOutliner {
         final float pt,
         final CallbackInfo ci
     ) {
-        if (outlines.size() > 100000) {
-            ci.cancel();
-        }
+        MixinOutlinerLogic.INSTANCE.preRenderOutlines$create_interactive(outlines, ci);
     }
 }
