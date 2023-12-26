0bf67441d4810e463aace3b8a3eb9fc570ea017e
StewStrong <stewstronger@gmail.com>
2023-12-26 06:43:34 -0700

    Fixed not being able to wrench trains on trains

--- common/src/main/kotlin/org/valkyrienskies/create_interactive/mixin_logic/client/MixinMinecraftLogic.kt
+++ common/src/main/kotlin/org/valkyrienskies/create_interactive/mixin_logic/client/MixinMinecraftLogic.kt
@@ -2,7 +2,6 @@ package org.valkyrienskies.create_interactive.mixin_logic.client
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation
 import com.simibubi.create.AllItems
-import com.simibubi.create.content.contraptions.AbstractContraptionEntity
 import com.simibubi.create.content.contraptions.ContraptionHandler
 import com.simibubi.create.content.contraptions.ContraptionHandlerClient
 import com.simibubi.create.content.trains.entity.CarriageContraptionEntity
@@ -16,8 +15,11 @@ import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.phys.AABB
 import net.minecraft.world.phys.BlockHitResult
+import org.valkyrienskies.core.api.ships.ClientShip
 import org.valkyrienskies.create_interactive.CreateInteractiveEventsClient.postTickClient
-import java.lang.ref.WeakReference
+import org.valkyrienskies.mod.common.getShipManagingPos
+import org.valkyrienskies.mod.common.util.toJOML
+import org.valkyrienskies.mod.common.util.toMinecraft
 
 internal object MixinMinecraftLogic {
     internal fun postTick(pause: Boolean, level: ClientLevel?, connection: ClientPacketListener?) {
@@ -26,6 +28,9 @@ internal object MixinMinecraftLogic {
         }
     }
 
+    /**
+     * Fix train wrenching by doing train wrenching after block interactions
+     */
     internal fun wrapUseItemOn(
         gameMode: MultiPlayerGameMode,
         player: LocalPlayer,
@@ -35,29 +40,38 @@ internal object MixinMinecraftLogic {
         operation: Operation<InteractionResult>,
     ): InteractionResult {
         val result = operation.call(gameMode, player, level, interactionHand, blockHitResult)
-        if (!result.consumesAction()) {
+        if (!result.consumesAction() && AllItems.WRENCH.isIn(player.getItemInHand(interactionHand))) {
             // Consider a train relocation
-            val contraptionsInLevel = ContraptionHandler.loadedContraptions.get(level).values
-            for (contraptionEntityRef: WeakReference<AbstractContraptionEntity> in contraptionsInLevel) {
-                val rayInputs = ContraptionHandlerClient.getRayInputs(player)
-                val origin = rayInputs.first
-                val target = rayInputs.second
-                val aabb = AABB(origin, target).inflate(16.0)
-                val contraptionEntity = contraptionEntityRef.get() ?: continue
-                if (!contraptionEntity.boundingBox.intersects(aabb))
-                    continue
+            val rayInputs = ContraptionHandlerClient.getRayInputs(player)
+            val origin = rayInputs.first
+            val target = rayInputs.second
 
-                val rayTraceResult =
-                    ContraptionHandlerClient.rayTraceContraption(origin, target, contraptionEntity) ?: continue
-                val pos = rayTraceResult.blockPos
+            val aabb = AABB(origin, target).inflate(16.0)
+
+            val contraptionsInLevel = ContraptionHandler.loadedContraptions.get(level).values
+
+            contraptionsInLevel.mapNotNull { it.get() as? CarriageContraptionEntity? }.forEach { contraptionEntity ->
+                val ship = level.getShipManagingPos(contraptionEntity.anchorVec)
+                if (ship != null) {
+                    val transform = if (ship is ClientShip) ship.renderTransform else ship.transform
+                    val newAABB = aabb.toJOML().transform(transform.worldToShip).toMinecraft()
+                    if (!contraptionEntity.boundingBox.intersects(newAABB)) {
+                        return@forEach
+                    }
+                } else {
+                    if (!contraptionEntity.boundingBox.intersects(aabb)) {
+                        return@forEach
+                    }
+                }
 
                 // Effectively do ContraptionHandlerClient.handleSpecialInteractions()
-                if (AllItems.WRENCH.isIn(player.getItemInHand(interactionHand)) && contraptionEntity is CarriageContraptionEntity) {
-                    TrainRelocator.carriageWrenched(
-                        contraptionEntity.toGlobalVector(VecHelper.getCenterOf(pos), 1.0f),
-                        contraptionEntity
-                    )
-                }
+                val rayTraceResult =
+                    ContraptionHandlerClient.rayTraceContraption(origin, target, contraptionEntity) ?: return@forEach
+
+                TrainRelocator.carriageWrenched(
+                    contraptionEntity.toGlobalVector(VecHelper.getCenterOf(rayTraceResult.blockPos), 1.0f),
+                    contraptionEntity
+                )
             }
         }
         return result
