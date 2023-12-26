5c31284593273df25a95b1c58de565798048fbef
StewStrong <stewstronger@gmail.com>
2023-12-26 12:43:57 -0700

    Prevent the outline renderer from freezing the game

---
+++ common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinOutliner.java
@@ -0,0 +1,34 @@
+package org.valkyrienskies.create_interactive.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.simibubi.create.foundation.outliner.Outliner;
+import com.simibubi.create.foundation.render.SuperRenderTypeBuffer;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+
+@Mixin(Outliner.class)
+public class MixinOutliner {
+    @Shadow
+    @Final
+    private Map<Object, Outliner.OutlineEntry> outlines;
+
+    @Inject(method = "renderOutlines", at = @At("HEAD"), cancellable = true)
+    private void preRenderOutlines(
+        final PoseStack ms,
+        final SuperRenderTypeBuffer buffer,
+        final Vec3 camera,
+        final float pt,
+        final CallbackInfo ci
+    ) {
+        if (outlines.size() > 100000) {
+            ci.cancel();
+        }
+    }
+}
