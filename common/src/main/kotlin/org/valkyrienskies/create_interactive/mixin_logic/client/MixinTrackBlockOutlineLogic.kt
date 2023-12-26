35e10768f101b3b797c78bd311ac28ddabab48ad
StewStrong <stewstronger@gmail.com>
2023-12-26 15:01:43 -0700

    Fixed proguard trying to build

--- common/src/main/kotlin/org/valkyrienskies/create_interactive/mixin_logic/client/MixinTrackBlockOutlineLogic.kt
+++ common/src/main/kotlin/org/valkyrienskies/create_interactive/mixin_logic/client/MixinTrackBlockOutlineLogic.kt
@@ -1,8 +1,8 @@
 package org.valkyrienskies.create_interactive.mixin_logic.client
 
 import com.simibubi.create.content.trains.track.TrackBlockOutline
-import com.simibubi.create.content.trains.track.TrackBlockOutline.result
 import org.valkyrienskies.core.api.ships.ClientShip
+import org.valkyrienskies.create_interactive.mixin.TrackBlockOutlineAccessor
 import org.valkyrienskies.mod.common.getShipManagingPos
 import org.valkyrienskies.mod.common.util.toJOML
 import org.valkyrienskies.mod.common.util.toMinecraft
@@ -12,11 +12,16 @@ internal object MixinTrackBlockOutlineLogic {
      * Fixed train station direction wrong on ships
      */
     internal fun postClientTick() {
-        val origResult = result ?: return
-        val clientShip = origResult.blockEntity.level.getShipManagingPos(origResult.blockEntity.blockPos) as? ClientShip ?: return
-        val newDirection = origResult.direction.toJOML().rotate(clientShip.renderTransform.shipToWorldRotation).toMinecraft()
-        result = TrackBlockOutline.BezierPointSelection(
-            origResult.blockEntity, origResult.loc, origResult.vec, origResult.angles, newDirection
+        val origResult = TrackBlockOutlineAccessor.getResult() ?: return
+        val clientShip =
+            origResult.blockEntity.level.getShipManagingPos(origResult.blockEntity.blockPos) as? ClientShip ?: return
+        val newDirection =
+            origResult.direction.toJOML().rotate(clientShip.renderTransform.shipToWorldRotation).toMinecraft()
+
+        TrackBlockOutlineAccessor.setResult(
+            TrackBlockOutline.BezierPointSelection(
+                origResult.blockEntity, origResult.loc, origResult.vec, origResult.angles, newDirection
+            )
         )
     }
 }
