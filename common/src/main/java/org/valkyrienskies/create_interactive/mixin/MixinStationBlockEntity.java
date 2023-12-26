42b747c717e7d2193d16d8108ee1abc8bf125b38
ewoudje <7384674+ewoudje@users.noreply.github.com>
2023-12-27 00:49:29 +0100

    Classic ewoudje, pushed something that didn't work :sunglasses:

--- common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinStationBlockEntity.java
+++ common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinStationBlockEntity.java
@@ -1,8 +1,11 @@
 package org.valkyrienskies.create_interactive.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.simibubi.create.content.trains.entity.CarriageContraption;
 import com.simibubi.create.content.trains.station.StationBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -12,9 +15,20 @@ public class MixinStationBlockEntity {
     // You aren't required to add controls to the trains since you can just add them afterwards
     @WrapOperation(
         method = "assemble",
-        at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/trains/entity/CarriageContraption;hasForwardControls()Z")
+        at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/trains/entity/CarriageContraption;hasForwardControls()Z"),
+        remap = false
     )
-    private boolean skipControls(boolean original) {
+    private boolean skipControls(CarriageContraption instance, Operation<Boolean> operation) {
+        return true;
+    }
+
+    @WrapOperation(
+        method = "assemble",
+        at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/trains/entity/CarriageContraption;assemble(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)Z"),
+        remap = false
+    )
+    private boolean emptyCarriage(CarriageContraption instance, Level level, BlockPos pos, Operation<Boolean> operation) {
+        operation.call(level, pos);
         return true;
     }
 }
