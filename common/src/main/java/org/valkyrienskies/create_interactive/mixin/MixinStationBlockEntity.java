2924cd065bf949c2cc9b0d8bf12408bf21504932
ewoudje <7384674+ewoudje@users.noreply.github.com>
2023-12-27 01:10:15 +0100

    Assembly works with only boggey

--- common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinStationBlockEntity.java
+++ common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinStationBlockEntity.java
@@ -4,8 +4,6 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.simibubi.create.content.trains.entity.CarriageContraption;
 import com.simibubi.create.content.trains.station.StationBlockEntity;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -19,16 +17,6 @@ public class MixinStationBlockEntity {
         remap = false
     )
     private boolean skipControls(CarriageContraption instance, Operation<Boolean> operation) {
-        return true;
-    }
-
-    @WrapOperation(
-        method = "assemble",
-        at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/trains/entity/CarriageContraption;assemble(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)Z"),
-        remap = false
-    )
-    private boolean emptyCarriage(CarriageContraption instance, Level level, BlockPos pos, Operation<Boolean> operation) {
-        operation.call(level, pos);
         return true;
     }
 }
