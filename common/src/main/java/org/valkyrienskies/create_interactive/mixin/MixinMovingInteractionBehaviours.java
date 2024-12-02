04130679167e495c3a966bf013ace04d6eee8003
xiewuzhiying <HoyhCreate@outlook.com>
2024-12-03 01:03:24 +0800

    I shouldn't do this.

--- common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinMovingInteractionBehaviours.java
+++ common/src/main/java/org/valkyrienskies/create_interactive/mixin/MixinMovingInteractionBehaviours.java
@@ -3,12 +3,9 @@ package org.valkyrienskies.create_interactive.mixin;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
-import com.simibubi.create.content.contraptions.actors.contraptionControls.ContraptionControlsMovingInteraction;
 import com.simibubi.create.content.contraptions.actors.seat.SeatInteractionBehaviour;
-import com.simibubi.create.content.contraptions.actors.trainControls.ControlsInteractionBehaviour;
 import com.simibubi.create.content.contraptions.behaviour.SimpleBlockMovingInteraction;
 import com.simibubi.create.content.kinetics.deployer.DeployerMovingInteraction;
-import com.simibubi.create.content.processing.burner.BlazeBurnerInteractionBehaviour;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
@@ -16,9 +13,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.valkyrienskies.create_interactive.mixinducks.AbstractContraptionEntityDuck;
 
 @Mixin(value = {
-        BlazeBurnerInteractionBehaviour.class,
-        ContraptionControlsMovingInteraction.class,
-        ControlsInteractionBehaviour.class,
         DeployerMovingInteraction.class,
         SeatInteractionBehaviour.class,
         SimpleBlockMovingInteraction.class
