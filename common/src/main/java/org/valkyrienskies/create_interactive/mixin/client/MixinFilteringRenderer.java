0a3a58f833cf7bb509b934fabfd891f5978f6ea6
xiewuzhiying <HoyhCreate@outlook.com>
2024-12-05 00:13:02 +0800

    If it's in an elevator, cancel the rendering

---
+++ common/src/main/java/org/valkyrienskies/create_interactive/mixin/client/MixinFilteringRenderer.java
@@ -0,0 +1,58 @@
+package org.valkyrienskies.create_interactive.mixin.client;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
+import com.simibubi.create.content.contraptions.actors.contraptionControls.ContraptionControlsBlock;
+import com.simibubi.create.content.contraptions.actors.contraptionControls.ContraptionControlsBlockEntity;
+import com.simibubi.create.content.contraptions.elevator.ElevatorContraption;
+import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
+import com.simibubi.create.foundation.blockEntity.behaviour.filtering.FilteringRenderer;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.create_interactive.CreateInteractiveUtil;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+
+@Mixin(FilteringRenderer.class)
+public abstract class MixinFilteringRenderer {
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/foundation/blockEntity/behaviour/BlockEntityBehaviour;get(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lcom/simibubi/create/foundation/blockEntity/behaviour/BehaviourType;)Lcom/simibubi/create/foundation/blockEntity/behaviour/BlockEntityBehaviour;"), cancellable = true)
+    private static void preTick(CallbackInfo ci, @Local(ordinal = 0) BlockState blockState, @Local(ordinal = 0) ClientLevel world, @Local(ordinal = 0) BlockPos pos) {
+        if (blockState.getBlock() instanceof ContraptionControlsBlock) {
+            final Ship ship = VSGameUtilsKt.getShipManagingPos(world, pos);
+            if (ship != null) {
+                final AbstractContraptionEntity contraption = CreateInteractiveUtil.INSTANCE.getContraptionEntityForShip(ship.getId(), world.isClientSide());
+                if (contraption != null && contraption.getContraption() instanceof ElevatorContraption) {
+                    ci.cancel();
+                }
+            }
+        }
+    }
+
+    @Inject(
+            method = "renderOnBlockEntity",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/simibubi/create/foundation/blockEntity/SmartBlockEntity;getBehaviour(Lcom/simibubi/create/foundation/blockEntity/behaviour/BehaviourType;)Lcom/simibubi/create/foundation/blockEntity/behaviour/BlockEntityBehaviour;"
+            )
+    )
+    private static void preRenderOnBlockEntity(SmartBlockEntity be, float partialTicks, PoseStack ms, MultiBufferSource buffer, int light, int overlay, CallbackInfo ci) {
+        if (be instanceof ContraptionControlsBlockEntity) {
+            final Level level = be.getLevel();
+            final Ship ship = VSGameUtilsKt.getShipManagingPos(level, be.getBlockPos());
+            if (ship != null) {
+                final AbstractContraptionEntity contraption = CreateInteractiveUtil.INSTANCE.getContraptionEntityForShip(ship.getId(), level.isClientSide());
+                if (contraption != null && contraption.getContraption() instanceof ElevatorContraption) {
+                    ci.cancel();
+                }
+            }
+        }
+    }
+}
