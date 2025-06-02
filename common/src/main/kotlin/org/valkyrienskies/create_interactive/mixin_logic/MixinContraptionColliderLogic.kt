cda4ed110fae5781628ed805b53dfb775c3744c6
blockninja124 <haganlgray@gmail.com>
2025-06-02 23:53:03 +0100

    Fix null crash when contraption takes a while to load

--- common/src/main/kotlin/org/valkyrienskies/create_interactive/mixin_logic/MixinContraptionColliderLogic.kt
+++ common/src/main/kotlin/org/valkyrienskies/create_interactive/mixin_logic/MixinContraptionColliderLogic.kt
@@ -5,8 +5,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 import org.valkyrienskies.create_interactive.CreateInteractiveUtil.doesContraptionHaveShipLoaded
 
 internal object MixinContraptionColliderLogic {
-    internal fun preCollideEntities(contraptionEntity: AbstractContraptionEntity, ci: CallbackInfo) {
+    internal fun preCollideEntities(contraptionEntity: AbstractContraptionEntity?, ci: CallbackInfo) {
         // Only disable collision if the contraption has a ship
+        if (contraptionEntity == null) return;
+
         if (doesContraptionHaveShipLoaded(contraptionEntity.contraption)) {
             ci.cancel()
         }
